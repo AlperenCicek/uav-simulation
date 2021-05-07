06c8e4845603acb81beea109d6dbae091abcc863
Alperen Çiçek <alperencicek1998@gmail.com>
2021-05-07 22:02:39 +0300

    AdditionOfTrackingSystem

--- UAV-Simulation/Assets/SimulationCodes/CarPathMovement.cs
+++ UAV-Simulation/Assets/SimulationCodes/CarPathMovement.cs
@@ -8,16 +8,25 @@ public class CarPathMovement : MonoBehaviour
     public GameObject path1;
     public GameObject path2;
     public GameObject path3;
+    public GameObject path4;
+    public GameObject path5;
+    public GameObject path6;
     public bool path1Flag;
     public bool path2Flag;
     public bool path3Flag;
+    public bool path4Flag;
+    public bool path5Flag;
+    public bool path6Flag;
 
     // Start is called before the first frame update
     void Start()
     {
         path1Flag = true;
         path2Flag = false;
-        
+        path3Flag = false;
+        path4Flag = false;
+        path5Flag = false;
+        path6Flag = false;
     }
 
     // Update is called once per frame
@@ -25,38 +34,85 @@ public class CarPathMovement : MonoBehaviour
     {
         //car.transform.LookAt(path1.transform);
         if(path1Flag)
-            car.transform.position = car.transform.position + new Vector3(0.001f, 0, 0);
-        else if(path2Flag)
-            car.transform.position = car.transform.position + new Vector3(0, 0, 0.001f);
-        else if(path3Flag)
-            car.transform.position = car.transform.position + new Vector3(-0.001f, 0, 0);
-
-        Debug.Log(car.transform.position);
-        if(path1Flag && path2Flag == false)
         {
-            if(0.2f > (car.transform.position.x - path1.transform.position.x))
+            car.transform.position = car.transform.position + new Vector3(0.001f, 0, 0);
+            if(0.2f > (path1.transform.position.x - car.transform.position.x))
             {
-                if(-0.2f < (car.transform.position.x - path1.transform.position.x))
+                if(-0.2f < (car.transform.position.z - path1.transform.position.z))
                 {
-                    car.transform.Rotate(new Vector3(0, 0, 90));
                     path1Flag = false;
                     path2Flag = true;
+                    car.transform.Rotate(new Vector3(0, 0, 270));
                 }
             }
         }
-
-        if(0.2f > (car.transform.position.x - path2.transform.position.x))
+        else if(path2Flag)
         {
-            if(-0.2f < (car.transform.position.x - path2.transform.position.x))
+            car.transform.position = car.transform.position + new Vector3(0, 0, -0.001f);
+            if(0.2f > (path2.transform.position.x - car.transform.position.x))
             {
-                if(path2Flag && path3Flag == false)
+                if(0.2f > (car.transform.position.z - path2.transform.position.z))
                 {
-                    car.transform.Rotate(new Vector3(0, 0, 90));
+                    car.transform.Rotate(new Vector3(0, 0, 270));
                     path2Flag = false;
                     path3Flag = true;
                 }
             }
         }
+        else if(path3Flag)
+        {
+            car.transform.position = car.transform.position + new Vector3(-0.001f, 0, 0);
+            if(0.2f < (path3.transform.position.x - car.transform.position.x))
+            {
+                if(0.2f > (car.transform.position.z - path3.transform.position.z))
+                {
+                    car.transform.Rotate(new Vector3(0, 0, 90));
+                    path3Flag = false;
+                    path4Flag = true;
+                }
+            }
+        }
+        else if(path4Flag)
+        {
+            car.transform.position = car.transform.position + new Vector3(0, 0, -0.001f);
+            if(0.5f > (path4.transform.position.x - car.transform.position.x))
+            {
+                if(0.5f > (car.transform.position.z - path4.transform.position.z))
+                {
+                    car.transform.Rotate(new Vector3(0, 0, 270));
+                    path4Flag = false;
+                    path5Flag = true;
+                }
+            }
+        }
+        else if(path5Flag)
+        {
+            car.transform.position = car.transform.position + new Vector3(-0.001f, 0, 0);
+            if(0.5f < (path5.transform.position.x - car.transform.position.x))
+            {
+                if(0.5f > (car.transform.position.z - path5.transform.position.z))
+                {
+                    car.transform.Rotate(new Vector3(0, 0, 270));
+                    path5Flag = false;
+                    path6Flag = true;
+                }
+            }
+        }
+        else if(path6Flag)
+        {
+            car.transform.position = car.transform.position + new Vector3(0, 0, 0.001f);
+            if(0.5f > (path6.transform.position.x - car.transform.position.x))
+            {
+                if(0.5f < (car.transform.position.z - path6.transform.position.z))
+                {
+                    car.transform.Rotate(new Vector3(0, 0, 270));
+                    path6Flag = false;
+                    path1Flag = true;
+                }
+            }
+        }
+
+        
             
     }
 }
